a36407b5da6b9fc88c26c0b4261f99257d22ab4a
Andris Plume <andris.plume@gmail.com>
2022-09-02 09:04:39 +0300

    Initial Commit

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Main {
+    public static void main(String[] args) {
+
+
+        PatRegistration patmeth = new PatRegistration();
+
+        patmeth.patRegistrationWindow();
+
+
+    }
+}
